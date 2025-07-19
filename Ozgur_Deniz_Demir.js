69eeb24628edcc1d2ad4ae0f4ce969d87b9dc92b
Özgür Deniz Demir <32364679+odenizddd@users.noreply.github.com>
2025-07-19 22:03:00 +0300

    feat: implement heart button

--- Ozgur_Deniz_Demir.js
+++ Ozgur_Deniz_Demir.js
@@ -24,6 +24,7 @@
     const productCount = productData.length;
     let productIndex = 0;
     let reachedEnd = false;
+    const favoriteProducts = [];
 
     // ## STATE ## end
 
@@ -48,6 +49,13 @@
     const PRODUCT_CAROUSEL_PREV_BUTTON_CLASSNAME = "product-carousel-prev-button" + UNIQUE_POSTFIX;
     const PRODUCT_CAROUSEL_NEXT_BUTTON_CLASSNAME = "product-carousel-next-button" + UNIQUE_POSTFIX;
     const PRODUCT_CAROUSEL_VIEWPORT_WRAPPER_CLASSNAME = "product-carousel-viewport-wrapper" + UNIQUE_POSTFIX;
+    const PRODUCT_CAROUSEL_ORIGINAL_PRICE_CLASSNAME = "product-carousel-original-price" + UNIQUE_POSTFIX;
+    const PRODUCT_CAROUSEL_DISCOUNT_PERCENTAGE_CLASSNAME = "product-carousel-discount-percentage" + UNIQUE_POSTFIX;
+    const PRODUCT_CAROUSEL_DISCOUNT_ICON_CLASSNAME = "product-carousel-discount-icon" + UNIQUE_POSTFIX;
+    const PRODUCT_CAROUSEL_DISCOUNT_CONTAINER_CLASSNAME = "product-carousel-discount-container" + UNIQUE_POSTFIX;
+    const PRODUCT_CAROUSEL_HEART_BUTTON_CLASSNAME = "product-carousel-heart-button" + UNIQUE_POSTFIX;
+    const NOT_VISIBLE_CLASSNAME = "not-visible" + UNIQUE_POSTFIX;
+
     // ## CONSTANTS ## end
 
     // ## STYLES ## start
@@ -60,6 +68,7 @@
             :root {
                 --primary-color-light: rgb(254, 246, 235);
                 --primary-color-dark: rgb(242, 142, 0);
+                --discount-color: rgb(0, 163, 101);
             }
 
             .${PRODUCT_CAROUSEL_CLASSNAME} {
@@ -108,6 +117,7 @@
                 margin: 20px 0px 20px 3px;
                 padding: 5px;
                 border-radius: 10px;
+                position: relative;
             }
 
             .${PRODUCT_CAROUSEL_CARD_CLASSNAME}:hover {
@@ -214,6 +224,66 @@
             .${PRODUCT_CAROUSEL_VIEWPORT_WRAPPER_CLASSNAME} {
                 position: relative;
             }
+
+            .${PRODUCT_CAROUSEL_ORIGINAL_PRICE_CLASSNAME} {
+                text-decoration: line-through;
+                color: rgb(125, 125, 125);
+                font-size: 14px;
+                font-weight: 500;
+                line-height: 22px;
+            }
+
+            .${NOT_VISIBLE_CLASSNAME} {
+                visibility: hidden;
+            }
+
+            .${PRODUCT_CAROUSEL_DISCOUNT_PERCENTAGE_CLASSNAME} {
+                color: var(--discount-color);
+                font-family: Poppins, "cursive";
+                font-size: 18px;
+                font-weight: 700;
+                line-height: 28.8px;
+                margin-left: 4.8px;
+            }
+
+            .${PRODUCT_CAROUSEL_DISCOUNT_ICON_CLASSNAME} {
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                background-color: var(--discount-color);
+                width: 22px;
+                height: 22px;
+                border-radius: 50%;
+                color: white;
+                margin-left: 4.8px;
+            }
+
+            .${PRODUCT_CAROUSEL_DISCOUNT_CONTAINER_CLASSNAME} {
+                display: flex;
+                align-items: center;
+            }
+
+            .${PRODUCT_CAROUSEL_HEART_BUTTON_CLASSNAME} {
+                width: 50px;
+                height: 50px;
+                background-color: white;
+                position: absolute;
+                top: 10px;
+                right: 15px;
+                box-shadow: rgba(0, 0, 0, 0.14) 0px 2px 4px 0px;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                border: none;
+                cursor: pointer;
+                border-radius: 50%;
+                color: var(--primary-color-dark);
+                padding: 0;
+            }
+
+            .${PRODUCT_CAROUSEL_HEART_BUTTON_CLASSNAME}:hover {
+                border: 1px solid var(--primary-color-dark);
+            }
         
         `;
         
@@ -248,23 +318,51 @@
         `;
     }
 
+    function getHeartButton() {
+        return `
+            <button class="${PRODUCT_CAROUSEL_HEART_BUTTON_CLASSNAME}">
+                <svg xmlns="http://www.w3.org/2000/svg" height="25" width="30" fill="none" viewBox="0 0 512 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path stroke="currentColor" stroke-width="50" d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z"/></svg>
+            </button>
+        `;
+    }
+
     function getProductCarouselCard({
         id, brand, name, url, img, price, original_price
     }) {
+
+        const hasDiscount = price !== original_price;
+        let discountPercentage = 0;
+
+        if (hasDiscount) {
+            discountPercentage = Math.round((original_price - price) / original_price * 100);
+        }
+
         return `
             <a href="${url}" target="_blank">
-                <div class="${PRODUCT_CAROUSEL_CARD_CLASSNAME}">
+                <div class="${PRODUCT_CAROUSEL_CARD_CLASSNAME}" id="${getHTMLIdFromProductId(id)}">
                     <img src="${img}" alt="${name}" class="${PRODUCT_CAROUSEL_IMAGE_CLASSNAME}">
                     <div class="${PRODUCT_CAROUSEL_PRODUCT_INFO_CLASSNAME}">
                         <div class="${PRODUCT_CAROUSEL_BRAND_AND_NAME_CLASSNAME}">
                             <span class="${PRODUCT_CAROUSEL_BRAND_CLASSNAME}">${brand} - </span>
                             <span class="${PRODUCT_CAROUSEL_NAME_CLASSNAME}">${name}</span>
                         </div>
+                            <div class="${!hasDiscount ? NOT_VISIBLE_CLASSNAME : ""} ${PRODUCT_CAROUSEL_DISCOUNT_CONTAINER_CLASSNAME}">
+                                <span class="${PRODUCT_CAROUSEL_ORIGINAL_PRICE_CLASSNAME}">
+                                    ${original_price} TL
+                                </span>
+                                <span class="${PRODUCT_CAROUSEL_DISCOUNT_PERCENTAGE_CLASSNAME}">
+                                    ${discountPercentage}%
+                                </span>
+                                <span class="${PRODUCT_CAROUSEL_DISCOUNT_ICON_CLASSNAME}">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" width="12" height="12" viewBox="0 0 384 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M169.4 470.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 370.8 224 64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 306.7L54.6 265.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z"/></svg>
+                                </span>
+                            </div>
                         <span class="${PRODUCT_CAROUSEL_PRICE_CLASSNAME}">${price} TL</span>
                     </div>
                     <div class="${PRODUCT_CAROUSEL_BUTTON_CONTAINER_CLASSNAME}">
                         <button class="${PRODUCT_CAROUSEL_BUTTON_CLASSNAME}">Sepete Ekle</button>
                     </div>
+                    ${getHeartButton()}
                 </div>
             </a>
         `;
@@ -361,6 +459,10 @@
         return node;
     }
 
+    function getHTMLIdFromProductId(productId) {
+        return `carousel-card-${productId}-${UNIQUE_POSTFIX}`;
+    }
+
     const setEventListeners = () => {
         const prevButton = document.querySelector(`.${PRODUCT_CAROUSEL_PREV_BUTTON_CLASSNAME}`);        
         prevButton.addEventListener("click", () => {
@@ -377,6 +479,31 @@
                 productIndex++;
                 updateProductCarousel();
             }
+        });
+
+        productData.forEach(product => {
+            const heartButton = document.querySelector(`#${getHTMLIdFromProductId(product.id)} .${PRODUCT_CAROUSEL_HEART_BUTTON_CLASSNAME}`);
+            const heartButtonIcon = heartButton.querySelector("svg");
+            
+            heartButton.addEventListener("click", (event) => {
+                event.preventDefault();
+                event.stopPropagation();
+
+                if (favoriteProducts.includes(product.id)) {
+                    favoriteProducts.splice(favoriteProducts.indexOf(product.id), 1);
+                } else {
+                    favoriteProducts.push(product.id);
+                }
+
+                console.log(heartButton)
+                console.log(heartButtonIcon);
+
+                if (favoriteProducts.includes(product.id)) {
+                    heartButtonIcon.setAttribute("fill", "currentColor");
+                } else {
+                    heartButtonIcon.setAttribute("fill", "none");
+                }
+            });
         });
         
     }
